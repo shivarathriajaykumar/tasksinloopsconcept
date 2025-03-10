45c7c6afdfbf3bd3092c824b4a0aa392780c9971
ajay <sajay82475@email.com>
2025-03-10 23:07:33 +0530

    first commit

---
+++ practice,string&arraymethods/node.js
@@ -0,0 +1,16 @@
+//template\string literals
+//'' => backticks
+// ${} =>interpollation ,in this both backticks and interpollations are writing content in this for dynamic content
+
+//let greet='morning'
+//let name='ajay kumar'
+
+//console.log(`hii good ${greet} ${name}`);
+
+
+//let num=200;
+//let num2=400;
+//console.log( `addition :${num+num2}`);
+
+//conditionnal statmenets
+
